5a2b3d9ff6f382d28b5e31d43bc9be92a2b4d49b
ramalla2020 <75368304+ramalla2020@users.noreply.github.com>
2020-12-02 20:09:12 +0530

    Update Game.java

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -2,7 +2,10 @@ package game;
 
 import casino.House;
 
-
+/*
+* @author Ramachandra Rao alla
+* @Description to start the game
+*/
 public abstract class Game {
     public abstract void start(House house);
 }
